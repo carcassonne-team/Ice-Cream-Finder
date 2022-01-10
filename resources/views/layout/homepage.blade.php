8a1d55a49a28219911dfe3b09f70e4d88aaf1fc5
mikolajgawronski <gawronski.miko@gmail.com>
2022-01-10 18:18:38 +0100

    #8 - changed homepage

--- resources/views/layout/homepage.blade.php
+++ resources/views/layout/homepage.blade.php
@@ -3,11 +3,9 @@
 @section('content')
     <div class="row justify-content-end">
         <div class="col-xl-8 detail-box">
-            <h1 class="col-10 text-end">Welcome to our Ice Cream Shop</h1>
+            <h1 class="col-10 text-end">Witamy w serwisie Ice Cream Finder</h1>
             <p class="text-end">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio omnis fugit, sed
-                tempora praesentium commodi error, hic recusandae repudiandae neque ad
-                molestias, atque veritatis labore quae eveniet autem in
+                Zapraszamy do rejestracji oraz do dodawania swoich lodziarni do naszego systemu, aby inni użytkownicy widzieli gdzie znajdują się lodziarnie, jakie lody oferują oraz aby wyrazili swoją opinię na ich temat!
             </p>
         </div>
 @endsection
